2ea1967cc1932a350eff765989747a8c0e8d050c
Zuko <tansautn@users.noreply.github.com>
2018-07-27 07:11:49 +0000

    Optimize update phpMyadmin

--- common/update-phpmyadmin.sh
+++ common/update-phpmyadmin.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-# Work on Debian & based Debian OS (Ubuntu) only
-
+### Only worked on Debian & Ubuntu
 function udtPhpMyAdmin
 {
 	function checkJq()
@@ -13,23 +12,35 @@ function udtPhpMyAdmin
 	}
 
 	function main {
-		versionUrl='https://www.phpmyadmin.net/home_page/version.json';
-		MyadmVer=$(curl $versionUrl -s | jq --raw-output '.version');
-        MyadmPath=/usr/share/phpmyadmin		
 		read -p "Update phpmyadmin to version :  $MyadmVer ? (Y/n)" isUdtMyadmin
 		if [[ "$isUdtMyadmin" != "n" ]] && [[ "$isUdtMyadmin" != "N" ]];
 		then
-			
+			checkJq;
 			cd ${MyadmPath};
+			if [ -e "${MyadmPath}-backup" ]; then rm -rf "${MyadmPath}-backup"; fi
+			echo "Backing-up your old phpMyAdmin directory !";
 			cp -rf ${MyadmPath} ${MyadmPath}-backup
-			echo "Downloading phpMyAdmin v$MyadmVer";
-			wget https://files.phpmyadmin.net/phpMyAdmin/${MyadmVer}/phpMyAdmin-${MyadmVer}-all-languages.zip;
-			unzip phpMyAdmin-${MyadmVer}-all-languages
+			echo "Downloading phpMyAdmin v$MyadmVer ---";
+			wget https://files.phpmyadmin.net/phpMyAdmin/${MyadmVer}/phpMyAdmin-${MyadmVer}-all-languages.zip -o;
+			unzip -o phpMyAdmin-${MyadmVer}-all-languages
 			cp -rf phpMyAdmin-${MyadmVer}-all-languages/* ${MyadmPath}
 			printf "\e[33m Update PHP-My-Admin : v$MyadmVer Done ! \e[0m \n "
 		fi
 	}
-	checkJq;
+	
+	function removeUnzipedFiles()
+	{
+		toRm=$(find ${MyadmPath} -iname "*all-languages");
+		echo $toRm;
+		for f in $toRm; do
+			echo Removing: $f;
+			rm -rf $f;
+		done
+	}
+	versionUrl='https://www.phpmyadmin.net/home_page/version.json';
+	MyadmVer=$(curl $versionUrl -s | jq --raw-output '.version');
+	MyadmPath=/usr/share/phpmyadmin		
 	main;
+	removeUnzipedFiles;
 }
 udtPhpMyAdmin;
