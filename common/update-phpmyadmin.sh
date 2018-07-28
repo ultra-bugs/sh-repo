10e04184ee6022c57120eb0eed920e1d3a5fc979
Zuko <tansautn@users.noreply.github.com>
2018-07-28 03:37:35 +0000

    Update update-phpmyadmin.sh

--- common/update-phpmyadmin.sh
+++ common/update-phpmyadmin.sh
@@ -12,19 +12,21 @@ function udtPhpMyAdmin
 	}
 
 	function main {
+	
 		versionUrl='https://www.phpmyadmin.net/home_page/version.json';
 		MyadmVer=$(curl $versionUrl -s | jq --raw-output '.version');
 		MyadmPath=/usr/share/phpmyadmin		
 		read -p "Update phpmyadmin to version :  $MyadmVer ? (Y/n)" isUdtMyadmin
 		if [[ "$isUdtMyadmin" != "n" ]] && [[ "$isUdtMyadmin" != "N" ]];
 		then
+			checkJq;
 			cd ${MyadmPath};
 			if [ -e "${MyadmPath}-backup" ]; then rm -rf "${MyadmPath}-backup"; fi
-			echo "Backing-up your old phpMyAdmin directory !";
+			echo "--- Backing-up your old phpMyAdmin directory ! ---";
 			cp -rf ${MyadmPath} ${MyadmPath}-backup
-			echo "Downloading phpMyAdmin v$MyadmVer ---";
-			wget https://files.phpmyadmin.net/phpMyAdmin/${MyadmVer}/phpMyAdmin-${MyadmVer}-all-languages.zip -o;
-			unzip -o phpMyAdmin-${MyadmVer}-all-languages
+			echo "--- Downloading phpMyAdmin v$MyadmVer ---";
+			wget https://files.phpmyadmin.net/phpMyAdmin/${MyadmVer}/phpMyAdmin-${MyadmVer}-all-languages.zip -o "${MyadmPath}/phpMyAdmin-${MyadmVer}-all-languages.zip";
+			unzip -o phpMyAdmin-${MyadmVer}-all-languages.zip
 			cp -rf phpMyAdmin-${MyadmVer}-all-languages/* ${MyadmPath}
 			printf "\e[33m Update PHP-My-Admin : v$MyadmVer Done ! \e[0m \n "
 		fi
@@ -39,7 +41,6 @@ function udtPhpMyAdmin
 			rm -rf $f;
 		done
 	}
-	checkJq;
 	main;
 	removeUnzipedFiles;
 }
