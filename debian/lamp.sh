dfb62a8e38a707a43ebe16f24d0c9a9b96e55ab3
Zuko <tansautn@users.noreply.github.com>
2020-12-02 15:20:03 +0700

    Add colorized header

--- debian/lamp.sh
+++ debian/lamp.sh
@@ -15,20 +15,22 @@
          *    -  -  All Rights Reserved  -  -    *
          * * * * * * * * * * * * * * * * * * * * *
 END
-echo $'
-         M""""""""`M            dP
-         Mmmmmm   .M            88
-         MMMMP  .MMM  dP    dP  88  .dP   .d8888b.
-         MMP  .MMMMM  88    88  88888"    88\'  `88
-         M\' .MMMMMMM  88.  .88  88  `8b.  88.  .88
-         M         M  `88888P\'  dP   `YP  `88888P\'
-         MMMMMMMMMMM    -*-  Created by Zuko  -*-
 
-         * * * * * * * * * * * * * * * * * * * * *
-
-         D.E.B.I.A.N  E.A.S.Y  L.A.M.P  S.T.A.C.K
-
+echo -e $'
+         \e[31mM""""""""`M            dP\e[0m
+         \e[31mMmmmmm   .M            88\e[0m
+         \e[31mMMMMP  .MMM  dP    dP  88  .dP   .d8888b.\e[0m
+         \e[31mMMP  .MMMMM  88    88  88888"    88\'  `88\e[0m
+         \e[31mM\' .MMMMMMM  88.  .88  88  `8b.  88.  .88\e[0m
+         \e[31mM         M  `88888P\'  dP   `YP  `88888P\'\e[0m
+         \e[31mMMMMMMMMMMM    \e[36m-*-\e[0m  \e[4;32mCreated by Zuko\e[0m  \e[36m-*-\e[0m\e[0m
+         
+         \e[35m* * * * * * * * * * * * * * * * * * * * *\e[0m
+         D.E.B.I.A.N  \e[32mE.A.S.Y\e[0m  L.A.M.P  \e[36mS.T.A.C.K\e[0m
+         \e[35m* * * * * * * * * * * * * * * * * * * * *\e[0m
+         \e[1;33m!  Just for running & serving the web  !\e[0m
          ';
+
 function getScriptDir
 {
     DIR="${BASH_SOURCE%/*}"
