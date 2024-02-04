5429180e1945dd5f7d6c2604362989097de1246c
Zuko <tansautn@users.noreply.github.com>
2024-02-04 19:21:03 +0700

    Remove PHP 8.x json package

--- debian/lamp.sh
+++ debian/lamp.sh
@@ -435,26 +435,26 @@ function insPhp73
 function insPhp74
 {
     echo "INFO :Installing PHP 7.4 (with FPM, No Apache Module) .........."
-    apt-get install php7.4 php7.4-bcmath php7.4-bz2 php7.4-cli php7.4-common php7.4-curl php7.4-fpm php7.4-gd php7.4-json php7.4-mbstring php7.4-mysql php7.4-opcache php7.4-readline php7.4-xml php7.4-zip -y
+    apt-get install php7.4 php7.4-bcmath php7.4-bz2 php7.4-cli php7.4-common php7.4-curl php7.4-fpm php7.4-gd php7.4-mbstring php7.4-mysql php7.4-opcache php7.4-readline php7.4-xml php7.4-zip -y
     enablePhp74
 }
 
 function insPhp80
 {
     echo "INFO :Installing PHP 7.4 (with FPM, No Apache Module) .........."
-    apt-get install php8.0 php8.0-bcmath php8.0-bz2 php8.0-cli php8.0-common php8.0-curl php8.0-fpm php8.0-gd php8.0-json php8.0-mbstring php8.0-mysql php8.0-opcache php8.0-readline php8.0-xml php8.0-zip -y
+    apt-get install php8.0 php8.0-bcmath php8.0-bz2 php8.0-cli php8.0-common php8.0-curl php8.0-fpm php8.0-gd php8.0-mbstring php8.0-mysql php8.0-opcache php8.0-readline php8.0-xml php8.0-zip -y
 }
 
 function insPhp81
 {
     echo "INFO :Installing PHP 7.4 (with FPM, No Apache Module) .........."
-    apt-get install php8.1 php8.1-bcmath php8.1-bz2 php8.1-cli php8.1-common php8.1-curl php8.1-fpm php8.1-gd php8.1-json php8.1-mbstring php8.1-mysql php8.1-opcache php8.1-readline php8.1-xml php8.1-zip -y
+    apt-get install php8.1 php8.1-bcmath php8.1-bz2 php8.1-cli php8.1-common php8.1-curl php8.1-fpm php8.1-gd php8.1-mbstring php8.1-mysql php8.1-opcache php8.1-readline php8.1-xml php8.1-zip -y
 }
 
 function insPhp82
 {
     echo "INFO :Installing PHP 7.4 (with FPM, No Apache Module) .........."
-    apt-get install php8.2 php8.2-bcmath php8.2-bz2 php8.2-cli php8.2-common php8.2-curl php8.2-fpm php8.2-gd php8.2-json php8.2-mbstring php8.2-mysql php8.2-opcache php8.2-readline php8.2-xml php8.2-zip -y
+    apt-get install php8.2 php8.2-bcmath php8.2-bz2 php8.2-cli php8.2-common php8.2-curl php8.2-fpm php8.2-gd php8.2-mbstring php8.2-mysql php8.2-opcache php8.2-readline php8.2-xml php8.2-zip -y
 }
 
 # COMPOSER
