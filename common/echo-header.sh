41da4e31cacb696250bfdc1dcb5436429b16957a
Zuko <tansautn@users.noreply.github.com>
2024-10-03 19:09:36 +0700

    For fun

--- common/echo-header.sh
+++ common/echo-header.sh
@@ -1,3 +1,26 @@
+# echo '';
+# echo '';
+# echo -e $'
+         # \e[31mM""""""""`M            dP\e[0m
+         # \e[31mMmmmmm   .M            88\e[0m
+         # \e[31mMMMMP  .MMM  dP    dP  88  .dP   .d8888b.\e[0m
+         # \e[31mMMP  .MMMMM  88    88  88888"    88\'  `88\e[0m
+         # \e[31mM\' .MMMMMMM  88.  .88  88  `8b.  88.  .88\e[0m
+         # \e[31mM         M  `88888P\'  dP   `YP  `88888P\'\e[0m
+         # \e[31mMMMMMMMMMMM    \e[36m-*-\e[0m  \e[4;32mCreated by Zuko\e[0m  \e[36m-*-\e[0m\e[0m
+         # ';
+
+# echo -e $'
+         # \e[35m* * * * * * * * * * * * * * * * * * * * *\e[0m
+         # D.E.B.I.A.N  \e[32mE.A.S.Y\e[0m  L.A.M.P  \e[36mS.T.A.C.K\e[0m
+         # \e[35m* * * * * * * * * * * * * * * * * * * * *\e[0m
+         # \e[1;33m!  Just for running & serving the web  !\e[0m
+         # ';
+# echo '';
+# echo '';
+
+# Funnies
+
 echo '';
 echo '';
 echo -e $'
@@ -14,7 +37,7 @@ echo -e $'
          \e[35m* * * * * * * * * * * * * * * * * * * * *\e[0m
          D.E.B.I.A.N  \e[32mE.A.S.Y\e[0m  L.A.M.P  \e[36mS.T.A.C.K\e[0m
          \e[35m* * * * * * * * * * * * * * * * * * * * *\e[0m
-         \e[1;33m!  Just for running & serving the web  !\e[0m
+         \e[1;33m!  You\'re lucky to still see me alive.  !\e[0m
          ';
 echo '';
 echo '';
